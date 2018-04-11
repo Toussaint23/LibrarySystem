be9ef3d6c533ee11484405b67c16e2a2210a2d31
Toussaint23 <36868582+Toussaint23@users.noreply.github.com>
2018-04-11 18:06:50 -0500

    check admin, Account and MemberAccountFactory

--- src/model/MemberAccountFactory.java
+++ src/model/MemberAccountFactory.java
@@ -3,7 +3,7 @@ package model;
 public class MemberAccountFactory {
 
 	public static MemberAccountImpl createMemberAndAccount(String id, String email, String firstName,
-			byte isSuperMember, String lastName, String user, String pwd) {
+			int isSuperMember, String lastName, String user, String pwd) {
 		Member member = new Member(id, email, firstName, isSuperMember, lastName);
 		Account account = new Account(user, pwd);
 
